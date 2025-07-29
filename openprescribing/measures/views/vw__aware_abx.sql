3d6ee34cf3f913fb933bf39ff6c42b7c8cd17d5b
Chris Wood <96439159+chrisjwood16@users.noreply.github.com>
2025-07-29 16:56:18 +0100

    Update vw__aware_abx.sql
    Update view to add bnf_code column.
    Fetch prescribing data as part of view.

--- openprescribing/measures/views/vw__aware_abx.sql
+++ openprescribing/measures/views/vw__aware_abx.sql
@@ -32,6 +32,7 @@ duplicate_vtms AS ( -- This produces a list of duplicate rows where a direct mat
 vmp_with_route AS (
   SELECT DISTINCT
     vmp.id AS id,
+    vmp.bnf_code AS bnf_code,
     vmp.vtm AS vtm,
     vmp.nm AS nm,
     COALESCE(sroute.route, routelookup.who_route) AS vmp_atc_route
@@ -49,28 +50,55 @@ vmp_with_route AS (
   AND ofr.descr NOT LIKE '%.gingival'
   AND ofr.descr != 'gel.vaginal'
   AND ofr.descr != 'cream.vaginal'
+),
+
+matched_vmps AS (
+  SELECT DISTINCT
+    matched_vtms.Antibiotic,
+    matched_vtms.nm as vtm_nm,
+    matched_vtms.id as vtm_id,
+    vmp.nm as vmp_nm,
+    vmp.id as vmp_id,
+    vmp.vmp_atc_route,
+    matched_vtms.atc_route,
+    matched_vtms.aware_2019,
+    matched_vtms.aware_2024
+  FROM matched_vtms
+  LEFT JOIN vmp_with_route vmp
+    ON matched_vtms.id = vmp.vtm
+    AND (
+      matched_vtms.atc_route IS NULL
+      OR matched_vtms.atc_route = vmp.vmp_atc_route
+    )
+  WHERE NOT EXISTS ( -- removes the duplicates identified above
+    SELECT 1
+    FROM duplicate_vtms
+    WHERE matched_vtms.Antibiotic = duplicate_vtms.Antibiotic AND matched_vtms.nm = duplicate_vtms.nm
+  ) AND vmp.id IS NOT NULL
+  ORDER BY matched_vtms.Antibiotic ASC;
 )
 
-SELECT DISTINCT
-  matched_vtms.Antibiotic,
-  matched_vtms.nm as vtm_nm,
-  matched_vtms.id as vtm_id,
-  vmp.nm as vmp_nm,
-  vmp.id as vmp_id,
-  vmp.vmp_atc_route,
-  matched_vtms.atc_route,
-  matched_vtms.aware_2019,
-  matched_vtms.aware_2024
-FROM matched_vtms
-LEFT JOIN vmp_with_route vmp
-  ON matched_vtms.id = vmp.vtm
-  AND (
-    matched_vtms.atc_route IS NULL
-    OR matched_vtms.atc_route = vmp.vmp_atc_route
-  )
-WHERE NOT EXISTS ( -- removes the duplicates identified above
-  SELECT 1
-  FROM duplicate_vtms
-  WHERE matched_vtms.Antibiotic = duplicate_vtms.Antibiotic AND matched_vtms.nm = duplicate_vtms.nm
-) AND vmp.id IS NOT NULL
-ORDER BY matched_vtms.Antibiotic ASC;
+SELECT
+    rx.month AS month, 
+    rx.practice AS practice, 
+    rx.bnf_name AS bnf_name,
+    rx.bnf_code AS bnf_code,
+    aware.aware_2024 AS aware,
+    SUM(quantity) AS quantity,
+    SUM(items) AS items
+FROM 
+    {project}.{hscic}.normalised_prescribing AS rx
+INNER JOIN 
+    {project}.{dmd}.vmp AS vmp
+    ON CONCAT(SUBSTR(rx.bnf_code, 0, 9), SUBSTR(rx.bnf_code, -2)) = CONCAT(SUBSTR(vmp.bnf_code, 0, 9), SUBSTR(vmp.bnf_code, -2)) --joins "generic" BNF code to dm+d
+INNER JOIN 
+    matched_vmps AS aware
+    ON SUBSTR(rx.bnf_code, 0, 9) = SUBSTR(matched_vmps.bnf_code, 0, 9)
+WHERE 
+    aware.aware_2024 IN ('Access', 'Watch', 'Reserve')
+GROUP BY 
+    month,
+    rx.practice,
+    rx.bnf_name, 
+    rx.bnf_code, 
+    aware.aware_2024
