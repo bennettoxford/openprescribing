ec151eb55da0565f8dc932ffe403fd1cab1edf44
Chris Wood <96439159+chrisjwood16@users.noreply.github.com>
2025-08-05 12:27:56 +0100

    Update view
    Add commenting
    Changed table name

--- openprescribing/measures/views/vw__aware_abx.sql
+++ openprescribing/measures/views/vw__aware_abx.sql
@@ -3,9 +3,9 @@ WITH aware_vmps AS (
     vmp.id AS vmp_id,
     vmp.nm AS vmp_nm,
     vmp.bnf_code AS bnf_code,
-    aware.aware_2019,
-    aware.aware_2024
-  FROM `{project}.{measures}.aware_vtms` aware
+    aware.aware_2019,  -- Historic AWaRE categories
+    aware.aware_2024 -- Current AWaRE categories
+  FROM `{project}.{measures}.tbl__aware_vtms` aware -- Table containing VTM IDs and their AWaRE categories
   INNER JOIN `{project}.{dmd}.vmp` vmp 
     ON aware.vtm_id = vmp.vtm
   INNER JOIN `{project}.{dmd}.ont` ont 
@@ -16,18 +16,18 @@ WITH aware_vmps AS (
     aware.atc_route IS NULL
     OR (
       aware.atc_route = 'P'
-      AND (
-        SUBSTR(ofr.descr, STRPOS(ofr.descr, '.') + 1) IN ('intravenous', 'intramuscular', 'intramuscular-deep')
-      )
+      AND SUBSTR(ofr.descr, STRPOS(ofr.descr, '.') + 1) -- Substring to get the route from the ontformroute description
+        IN ('intravenous', 'intramuscular', 'intramuscular-deep') -- Where the AWaRe category is defined with a 'P' route match only these dmd forms
     )
     OR (
       aware.atc_route = 'O'
-      AND ofr.descr LIKE '%.oral'
+      AND ofr.descr LIKE '%.oral'  -- Where the AWaRe category is defined with a 'O' route match only these dmd forms
     )
   )
-  AND SUBSTR(ofr.descr, STRPOS(ofr.descr, '.') + 1) IN ('inhalation', 'intramuscular', 'intramuscular-deep', 'intravenous', 'oral', 'vaginal', 'gastroenteral', 'rectal')
-  AND ofr.descr != 'gel.vaginal'
-  AND ofr.descr != 'cream.vaginal'
+  AND SUBSTR(ofr.descr, STRPOS(ofr.descr, '.') + 1)  -- Substring to get the route from the ontformroute description
+    IN ('inhalation', 'intramuscular', 'intramuscular-deep', 'intravenous', 'oral', 'vaginal', 'gastroenteral', 'rectal') -- Only include systemic forms that are relevant for AWaRe categorisation
+  AND ofr.descr != 'gel.vaginal' -- Exclude 'gel.vaginal' as considered more for localised effect
+  AND ofr.descr != 'cream.vaginal' -- Exclude 'cream.vaginal' as considered more for localised effect
 )
 
 SELECT
