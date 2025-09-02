d70c576333061ebfad6fae2547b9df9ce26bf4ef
Richard Croker <ra.croker@gmail.com>
2025-09-02 10:21:24 +0100

    Create vw__presentation_ingredient_count.sql

---
+++ openprescribing/measures/views/vw__presentation_ingredient_count.sql
@@ -0,0 +1,22 @@
+-- This view describes the number of separate ingredients in each presentation.  It's useful to have for areas like identifying where inhalers have multiple ingredients.
+SELECT
+  presentation,
+  presentation_code,
+  COUNT(ing) AS ing_count
+FROM
+  dmd.vpi vpi
+INNER JOIN
+  dmd.vmp vmp
+ON
+  vmp.id = vpi.vmp
+INNER JOIN
+  hscic.bnf AS bnf
+ON
+  CONCAT(SUBSTR(bnf.presentation_code, 0, 9),'AA',SUBSTR(bnf.presentation_code,-2, 2)) = CONCAT(SUBSTR(vmp.bnf_code, 0, 11),SUBSTR(vmp.bnf_code,-2, 2))
+WHERE
+  presentation_code LIKE '03%'
+GROUP BY
+  presentation,
+  presentation_code
+HAVING
+  COUNT(ing) >2
