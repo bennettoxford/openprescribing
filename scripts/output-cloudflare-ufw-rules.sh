f2089714ebe7fcca06cf8c7d2f302221e5c31b4f
Peter Inglesby <peter.inglesby@gmail.com>
2025-09-16 17:03:56 +0100

    Tighten firewall rules

--- scripts/output-cloudflare-ufw-rules.sh
+++ scripts/output-cloudflare-ufw-rules.sh
@@ -2,5 +2,5 @@
 set -euo pipefail
 
 for ip in $(curl -s https://www.cloudflare.com/ips-v4) $(curl -s https://www.cloudflare.com/ips-v6); do
-  echo "sudo ufw allow from $ip comment 'Cloudflare IP'"
+  echo "sudo ufw allow proto tcp from $ip to any port 443 comment 'Cloudflare IP'"
 done
