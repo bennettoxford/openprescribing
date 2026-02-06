f6f9803f3f6246cb331ec952c9b4ecd69a6fc87a
Tom Ward <tomward@fmail.co.uk>
2026-02-06 11:41:38 +0000

    Add a 4m timeout to refresh_news_feed_cache
    * typically takes only a couple of seconds to run
    * recently failed in an unknown way, in which it was using 100% cpu for 11 days

--- deploy/refresh_news_feed_cache.sh
+++ deploy/refresh_news_feed_cache.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 . /webapps/openprescribing/.venv/bin/activate
-python /webapps/openprescribing/openprescribing/manage.py refresh_news_feed_cache
+# This was recently observed failing & running for 11 days.
+# Our cronjob currently re-runs it every 5mins, and it's safe to kill it,
+# so let's add `timeout` as an insurance policy.
+timeout 4m python /webapps/openprescribing/openprescribing/manage.py refresh_news_feed_cache
