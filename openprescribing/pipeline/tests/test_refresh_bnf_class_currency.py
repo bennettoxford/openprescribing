810f92a8391dda05c09449207f700f3c8d906e81
Dave Evans <dave.evans@thedatalab.org>
2026-09-07 19:34:52 +0100

    Remove `is_current` setting from test fixtures
    The original status is no longer used to set the new status and leaving
    it in is just misleading.

--- openprescribing/pipeline/tests/test_refresh_bnf_class_currency.py
+++ openprescribing/pipeline/tests/test_refresh_bnf_class_currency.py
@@ -9,25 +9,25 @@ from matrixstore.tests.data_factory import DataFactory
 
 class TestCommand(TestCase):
     def test_refresh_bnf_class_currency(self):
-        Section.objects.create(bnf_id="01", bnf_chapter=1, is_current=True)
-        Section.objects.create(bnf_id="02", bnf_chapter=2, is_current=True)
-        Section.objects.create(bnf_id="03", bnf_chapter=3, is_current=False)
-        Section.objects.create(bnf_id="04", bnf_chapter=4, is_current=False)
+        Section.objects.create(bnf_id="01", bnf_chapter=1)
+        Section.objects.create(bnf_id="02", bnf_chapter=2)
+        Section.objects.create(bnf_id="03", bnf_chapter=3)
+        Section.objects.create(bnf_id="04", bnf_chapter=4)
 
-        Chemical.objects.create(bnf_code="010101001", is_current=True)
-        Chemical.objects.create(bnf_code="020101001", is_current=True)
-        Chemical.objects.create(bnf_code="030101001", is_current=False)
-        Chemical.objects.create(bnf_code="040101001", is_current=False)
+        Chemical.objects.create(bnf_code="010101001")
+        Chemical.objects.create(bnf_code="020101001")
+        Chemical.objects.create(bnf_code="030101001")
+        Chemical.objects.create(bnf_code="040101001")
 
-        Product.objects.create(bnf_code="010101001AA", is_current=True)
-        Product.objects.create(bnf_code="020101001AA", is_current=True)
-        Product.objects.create(bnf_code="030101001AA", is_current=False)
-        Product.objects.create(bnf_code="040101001AA", is_current=False)
+        Product.objects.create(bnf_code="010101001AA")
+        Product.objects.create(bnf_code="020101001AA")
+        Product.objects.create(bnf_code="030101001AA")
+        Product.objects.create(bnf_code="040101001AA")
 
-        Presentation.objects.create(bnf_code="010101001AAAAAA", is_current=True)
-        Presentation.objects.create(bnf_code="020101001AAAAAA", is_current=True)
-        Presentation.objects.create(bnf_code="030101001AAAAAA", is_current=False)
-        Presentation.objects.create(bnf_code="040101001AAAAAA", is_current=False)
+        Presentation.objects.create(bnf_code="010101001AAAAAA")
+        Presentation.objects.create(bnf_code="020101001AAAAAA")
+        Presentation.objects.create(bnf_code="030101001AAAAAA")
+        Presentation.objects.create(bnf_code="040101001AAAAAA")
 
         factory = DataFactory()
         factory.create_prescribing_for_bnf_codes(["010101001AAAAAA", "030101001AAAAAA"])
